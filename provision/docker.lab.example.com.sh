61243187738f0e0a1e9ee550ab523b65e35d2a40
asquelt <asq@asq.art.pl>
2017-04-21 12:37:58 +0200

    preliminary version of azure deployment

--- provision/docker.lab.example.com.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[ -f /vagrant/provision/ansible.sh ] && bash /vagrant/provision/ansible.sh
-[ -f /vagrant/provision/keys.sh ] && bash /vagrant/provision/keys.sh
-
-yum -d0 -y install docker
-
-docker version
-docker ps
